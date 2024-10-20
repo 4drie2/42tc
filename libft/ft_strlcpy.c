cc2563ba5ff5b2e45e59c4e11a0b22bf46d8a0b4
Adrien Zenbook <adr.Bidaux@gmail.com>
2024-10-20 18:06:29 +0200

    zenbook 20.3

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -12,13 +12,30 @@
 
 #include "libft.h"
 
-extern t_size strlcpy(char *dst, const char *src, t_size size)
+extern t_size ft_strlcpy(char *dst, const char *src, t_size size)
 {
     t_size i;
 
-    i = 0;
-    while (i <= size)
+    i = -1;
+    while (++i < size)
     {
-        
+        dst[i] = src[i];
     }
+    dst[size] = '\0';
+}
+
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char src1[] = "Vive les licornes";
+    char src2[] = "Vive les licornes";
+    char dst1[18];
+    char dst2[18];
+
+    strlpy(dst1, src1, 18);
+    ft_strlpy(dst2, src2, 18);
+    printf("   strlcpy :%s\nft_strlcpy:%s", dst1, dst2);
+    return 0;
 }
