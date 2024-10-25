3e295d14b9bf3cdd06873fc5a88fcffacddb04f8
Adrien Bidaux <abidaux@c2r11p6.42lehavre.fr>
2024-10-25 17:19:03 +0200

    juste au cas ou

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,33 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/24 14:16:20 by abidaux           #+#    #+#             */
-/*   Updated: 2024/10/25 17:02:05 by abidaux          ###   ########.fr       */
+/*   Created: 2024/10/25 14:17:43 by abidaux           #+#    #+#             */
+/*   Updated: 2024/10/25 16:55:56 by abidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-char	*ft_substr(char const *s, unsigned int start, t_size len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
+	t_size	size_s1;
+	t_size	size_s2;
 	char	*dest;
-	t_size	i;
 
-	i = 0;
-	if (!s)
+	if (!s1 || !s2)
 		return (0);
-	if (start >= ft_strlen(s))
-		return (ft_strdup(""));
-	if (len > ft_strlen(s) - start)
-		len = ft_strlen(s) - start;
-	dest = (char *)malloc(len + 1);
+	size_s1 = ft_strlen(s1);
+	size_s2 = ft_strlen(s2);
+	dest = (char *)malloc(size_s1 + size_s2 + 1);
 	if (!dest)
 		return (0);
-	ft_strlcpy(dest, s + start, len + 1);
+	ft_strlcpy(dest, s1, (size_s1 + size_s2 + 1));
+	ft_strlcat(dest, (char *)s2, (size_s1 + size_s2 + 1));
 	return (dest);
 }
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,33 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/24 14:16:20 by abidaux           #+#    #+#             */
-/*   Updated: 2024/10/25 17:02:05 by abidaux          ###   ########.fr       */
+/*   Created: 2024/10/25 17:17:43 by abidaux           #+#    #+#             */
+/*   Updated: 2024/10/25 17:18:46 by abidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <stdlib.h>
-
-char	*ft_substr(char const *s, unsigned int start, t_size len)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*dest;
-	t_size	i;
 
-	i = 0;
-	if (!s)
-		return (0);
-	if (start >= ft_strlen(s))
-		return (ft_strdup(""));
-	if (len > ft_strlen(s) - start)
-		len = ft_strlen(s) - start;
-	dest = (char *)malloc(len + 1);
-	if (!dest)
-		return (0);
-	ft_strlcpy(dest, s + start, len + 1);
-	return (dest);
 }
