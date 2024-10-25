4a500a7dfc90eadb48ccea8a4d5ce1c8a839e443
Adrien Bidaux <abidaux@c2r11p6.42lehavre.fr>
2024-10-25 18:06:38 +0200

    euh on mange quand ?

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,11 +6,49 @@
 /*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 17:17:43 by abidaux           #+#    #+#             */
-/*   Updated: 2024/10/25 17:18:46 by abidaux          ###   ########.fr       */
+/*   Updated: 2024/10/25 17:54:45 by abidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <stdlib.h>
+
+/* char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		start;
+	int		end;
+	char	*dest;
+
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && ft_mem(set, s1[start]))
+		start++;
+	while (ft_strncmp(s1 + (end - ft_strlen(set)), set, ft_strlen(set)))
+		end -= ft_strlen(set);
+	dest = (char *)malloc(ft_strlen(s1) - (start + end));
+	if (!dest)
+		return (0);
+	ft_strlcpy(dest, s1 + start, ft_strlen(s1) - (start + end));
+	return (dest);
+} */
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	int		start;
+	int		end;
+	char	*dest;
 
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end - 1]))
+		end--;
+	dest = (char *)malloc(end - start + 1);
+	if (!dest)
+		return (NULL);
+	ft_strlcpy(dest, s1 + start, end - start + 1);
+	return (dest);
 }
