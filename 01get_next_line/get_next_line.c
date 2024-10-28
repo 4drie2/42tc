98da34616a3e8565065320f83955cf12bc340c7b
Adrien 42 <adr.bidaux@gmail.com>
2024-10-28 15:31:54 +0100

    dur

--- 01get_next_line/get_next_line.c
+++ 01get_next_line/get_next_line.c
@@ -6,8 +6,34 @@
 /*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 20:02:11 by abidaux           #+#    #+#             */
-/*   Updated: 2024/10/27 20:02:45 by abidaux          ###   ########.fr       */
+/*   Updated: 2024/10/28 15:25:31 by abidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include "../00libft/libft.h"
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 32
+#endif
+
+int	main(void)
+{
+	char	*buffer;
+	int		nread;
+	int		fd;
+
+	fd = open("hello.txt", O_RDONLY);
+	buffer = (char *)malloc(sizeof(char) * (int)BUFFER_SIZE);
+	if (!buffer)
+		return (0);
+	nread = read(fd, buffer, 32);
+	if (nread == -1)
+		ft_putstr_fd("Erreur", 1);
+	ft_putstr_fd("read=", 1);
+	ft_putnbr_fd(nread, 1);
+	ft_putchar_fd('\n', 1);
+	ft_putstr_fd(buffer, 1);
+	close(fd);
+	return (0);
+}
--- 01get_next_line/get_next_line.h
+++ 01get_next_line/get_next_line.h
@@ -6,9 +6,13 @@
 /*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 20:02:17 by abidaux           #+#    #+#             */
-/*   Updated: 2024/10/27 20:04:08 by abidaux          ###   ########.fr       */
+/*   Updated: 2024/10/28 15:08:48 by abidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+char	*get_next_line(int fd);
