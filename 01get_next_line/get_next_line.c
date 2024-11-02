0a65f5241c6374560f7c11e45c2983dacc1b2eb1
Adrien 42 <adr.bidaux@gmail.com>
2024-11-02 18:00:09 +0100

    PIZZAAAA ?

--- 01get_next_line/get_next_line.c
+++ 01get_next_line/get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abidaux <abidaux@student.42lehavre.fr>     +#+  +:+       +#+        */
+/*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 20:02:11 by abidaux           #+#    #+#             */
-/*   Updated: 2024/11/02 00:58:48 by abidaux          ###   ########.fr       */
+/*   Updated: 2024/11/02 17:59:40 by abidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,29 @@
 # define BUFFER_SIZE 32
 #endif
 
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*dest;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if (start >= ft_strlen(s))
+		return (0);
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	dest = (char *)malloc(len + 1);
+	if (!dest)
+		return (0);
+	ft_strlcpy(dest, s + start, len + 1);
+	return (dest);
+}
+
 char	*gnl(int fd)
 {
 	char		*buffer;
+	char		*temp;
 	static char	*stash;
 	int			nread;
 
@@ -36,8 +56,13 @@ char	*gnl(int fd)
 		stash = ft_strjoin(stash, buffer);
 		printf("read=%i, buffer=%s\n", nread, buffer); ///
 	}
+	printf("\npretemp=|%s|\n", ft_strchr(stash, '\n'));
+	temp = stash;
+	stash = malloc(ft_strlen(stash) - ft_strlen(ft_strchr(stash, '\n')) + 1);
+	ft_strlcpy(stash, ft_strchr(stash, '\n'), ft_strlen(ft_strchr(stash, '\n')));
+	printf("temp=|%s|\n" ,temp);
 	printf("stash=|%s|\n" ,stash);
-	return (stash);
+	return (temp);
 }
 
 int main(void)
