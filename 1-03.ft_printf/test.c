61c3bd941218f68f869075d16d602bc16843e8cb
Adrien 42 <adr.bidaux@gmail.com>
2024-11-15 01:22:15 +0100

    DODO

--- 1-03.ft_printf/test.c
+++ 1-03.ft_printf/test.c
@@ -1,15 +1,10 @@
-#include <stdio.h>
-#include <stdarg.h>
+// #include <stdio.h>
+// #include <stdarg.h>
 #include <unistd.h>
 
 int main()
 {
-	char *god = "d";
-
-	printf("%s\n", god);
-	int i = 0;
-	while (god[i])
-	{
-		write(1, &god[i++], 1);
-	}
+	char *god = "Hello!";
+	write(1, god, 6);
+	return 0;
 }
