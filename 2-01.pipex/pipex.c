f954ba3e338916364267a44181b68687b178cfa2
Adrien 42 <adr.bidaux@gmail.com>
2024-11-19 21:37:45 +0100

    file1->X file2->OK argv->OK

--- 2-01.pipex/pipex.c
+++ 2-01.pipex/pipex.c
@@ -6,14 +6,48 @@
 /*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 20:22:46 by abidaux           #+#    #+#             */
-/*   Updated: 2024/11/19 20:07:54 by abidaux          ###   ########.fr       */
+/*   Updated: 2024/11/19 21:36:21 by abidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "pipex.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+
+void	luke(int pipefd[], char **argv)
+{
+	close(pipefd[0]);
+	if (dup2(pipefd[1], 1) == -1)
+		return((void)write(2, "erreur 2", 8));
+	int fd = open(argv[1], O_RDONLY);
+	if (dup2(fd, 0) == -1)
+		return((void)write(2, "erreur 3", 8));
+	char **cmd = ft_split(argv[2], ' ');
+	char *root = ft_strjoin("/bin/", cmd[0]);
+	char *envp[] = {NULL};
+	if (execve(root, cmd, envp) == -1)
+		return((void)write(2, "erreur 4", 8));
+	close(pipefd[1]);
+}
+
+void	vador(int pipefd[], char **argv)
+{
+	close(pipefd[1]);
+	if (dup2(pipefd[0], 0) == -1)
+		return((void)write(2, "erreur 4", 8));
+	int fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (dup2(fd, 1) == -1)
+		return((void)write(2, "erreur 5", 8));
+	char **cmd = ft_split(argv[3], ' ');
+	char *root = ft_strjoin("/bin/", cmd[0]);
+	char *envp[] = {NULL};
+	if (execve(root, cmd, envp) == -1)
+		return((void)write(2, "erreur 6", 8));
+	close(pipefd[0]);
+}
 
 int main (int argc, char **argv)
 {
@@ -28,27 +62,14 @@ int main (int argc, char **argv)
 		return(write(2, "erreur 1", 8), 0);
 	if (pid == 0)
 	{
-		close (pipefd[0]);
-		if (dup2(pipefd[1], 1) == -1)
-			return(write(2, "erreur 2", 8), 0);
-		char *root ="/bin/ls"; // root
-		char *cmd[] = {"ls", NULL};
-		char *envp[] = {NULL};
-		if (execve(root, cmd, envp) == -1)
-			return(write(2, "erreur 3", 8), 0);
-		close(pipefd[1]);
+		luke(pipefd, argv);
 	}
 	else
 	{
-		close(pipefd[1]);
-		if (dup2(pipefd[0], 0) == -1)
-			return(write(2, "erreur 3", 8), 0);
-		char *root = "/bin/grep";
-		char *cmd[] = {"grep", ".c", NULL};
-		char *envp[] = {NULL};
-		if (execve(root, cmd, envp) == -1)
-			return (write(2, "erreur 8\n", 9), 1);
-		close(pipefd[0]);
+		vador(pipefd, argv);
+		waitpid(pid, NULL, 0);
 	}
 	return (0);
 }
+
+
---
+++ 2-01.pipex/pipex.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/19 20:18:10 by abidaux           #+#    #+#             */
+/*   Updated: 2024/11/19 21:28:38 by abidaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "../00libft/libft.h"
+
