fb4ab91349bfdb1114bdf6e07cded935ff1b4bec
Adrien 42 <adr.bidaux@gmail.com>
2024-11-30 01:17:25 +0100

    chelou

--- 2-01.pipex/test.c
+++ 2-01.pipex/test.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
+// int main(void)
+// {
+// 	char *root = "/tmp";
+// 	printf("access: %d\n", access(root, F_OK));
+// 	if (access(root, F_OK) == 0)
+// 		printf("unlink: %d", unlink("tmp"));
+// 	return (0);
+// }
+
 int main(void)
 {
-	printf("%i", access("/bin/ls", X_OK));
+	if (access("/tmp", F_OK) == 0)
+		printf("unlink: %d", unlink("tmp"));
+	return (0);
 }
