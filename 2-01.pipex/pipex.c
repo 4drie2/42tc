8cc2b7566a4b3d4e9c05ef3a30a0ce68e712bd44
Adrien 42 <adr.bidaux@gmail.com>
2024-12-04 18:44:13 +0100

    move on pipex

--- 2-01.pipex/pipex.c
+++ 2-01.pipex/pipex.c
@@ -5,21 +5,43 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/18 20:22:46 by abidaux           #+#    #+#             */
-/*   Updated: 2024/12/02 20:31:29 by abidaux          ###   ########.fr       */
+/*   Created: 2024/12/04 17:09:43 by abidaux           #+#    #+#             */
+/*   Updated: 2024/12/04 18:10:52 by abidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char **get_paths(char **envp)
+char	*get_path(char *cmd, char **envp)
 {
-	while (*envp && !(ft_strncmp(*envp, "PATH=", 5) == 0))
+	char	**paths;
+	char	*postpath;
+	char	*path;
+	int		i;
+
+	i = -1;
+	while (*envp && ft_strncmp(*envp, "PATH=", 5))
 		++envp;
-	return (ft_split(*envp + 5, ':'));
+	paths = ft_split(*envp + 5, ':');
+	postpath = ft_strjoin("/", cmd);
+	while (paths[++i])
+	{
+		path = ft_strjoin(paths[i], postpath);
+		if (access(path, X_OK) == 0)
+			break ;
+		free(path);
+		path = NULL;
+	}
+	free(postpath);
+	i = -1;
+	while (paths[++i])
+		free(paths[i]);
+	if (!path)
+		return (free(paths), perror("cmd not found in PATH"), NULL);
+	return (free(paths), path);
 }
 
-void	luke(int pipefd[], char **argv)
+void	luke(int pipefd[], char **argv, char **envp)
 {
 	char	**cmd;
 	char	*root;
@@ -28,21 +50,21 @@ void	luke(int pipefd[], char **argv)
 	close(pipefd[0]);
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
-		return ((void)write(2, "erreur 1 - file cannot be read", 30));
+		return ((void)perror("erreur 1 - file cannot be read"));
 	if (dup2(fd, 0) == -1)
-		return ((void)write(2, "erreur 2 - file->fd[0]", 22));
+		return ((void)perror("erreur 2 - file->fd[0]"));
 	if (dup2(pipefd[1], 1) == -1)
-		return ((void)write(2, "erreur 3 - fd[1]->pipe[1]", 25));
+		return ((void)perror("erreur 3 - fd[1]->pipe[1]"));
 	cmd = ft_split(argv[2], ' ');
-	root = ft_strjoin("/bin/", cmd[0]);
-	if (execve(root, cmd, NULL) == -1)
-		return ((void)write(2, "erreur 4 - execve 1 impossibe", 29));
+	root = get_path(cmd[0], envp);
+	if (execve(root, cmd, envp) == -1)
+		return ((void)perror("erreur 4 - execve 1 impossibe"));
 	close(pipefd[1]);
 	close(fd);
 	return (free(root), free(cmd));
 }
 
-void	vador(int pipefd[], char **argv)
+void	vador(int pipefd[], char **argv, char **envp)
 {
 	char	**cmd;
 	char	*root;
@@ -50,40 +72,38 @@ void	vador(int pipefd[], char **argv)
 
 	close(pipefd[1]);
 	if (dup2(pipefd[0], 0) == -1)
-		return ((void)write(2, "erreur 4", 8));
+		return ((void)perror("erreur 4"));
 	fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
-		return ((void)write(2, "erreur 1.2 - file cannot be read", 32));
+		return ((void)perror("erreur 1.2 - file cannot be read"));
 	if (dup2(fd, 1) == -1)
-		return ((void)write(2, "erreur 5", 8));
+		return ((void)perror("erreur 5"));
 	cmd = ft_split(argv[3], ' ');
-	root = ft_strjoin("/bin/", cmd[0]);
-	if (execve(root, cmd, NULL) == -1)
-		return ((void)write(2, "erreur 6", 8));
+	root = get_path(cmd[0], envp);
+	if (execve(root, cmd, envp) == -1)
+		return ((void)perror("erreur 6"));
 	close(pipefd[0]);
 	close(fd);
 	return (free(root), free(cmd));
 }
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
 	int	pipefd[2];
 	int	pid;
 
-	//if (argc != 5)
-	//	return (write(2, "erreur 0", 8), 0);
-	if (argc != 5 && (!argv[1] || !argv[2] || !argv[3] || !argv[4]))
-		return (write(2, "Invalid arguments\n", 18), 0);
+	if (argc != 5 && (!argv[1] || !argv[2] || !argv[3] || !argv[4] || !envp))
+		return (perror("Invalid arguments"), 0);
 	if (pipe(pipefd) == -1)
-		return (write(2, "erreur 0", 8), 0);
+		return (perror("erreur 0"), 0);
 	pid = fork();
 	if (pid == -1)
-		return (write(2, "erreur 1", 8), 0);
+		return (perror("erreur 1"), 0);
 	if (pid == 0)
-		luke(pipefd, argv);
+		luke(pipefd, argv, envp);
 	else
 	{
-		vador(pipefd, argv);
+		vador(pipefd, argv, envp);
 		waitpid(pid, NULL, 0);
 	}
 	return (0);
