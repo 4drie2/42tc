b2989385aa9acbdf2f80ca9b5fb4067f040cbc97
Adrien 42 <adr.bidaux@gmail.com>
2025-01-31 20:11:55 +0100

    rosting ok

--- 2-exam02/Level 4/rostring/rostring.c
+++ 2-exam02/Level 4/rostring/rostring.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
-void write_word(char *start, char *end)
+/* void write_word(char *start, char *end)
 {
 	while (start < end)
 	{
@@ -48,18 +48,28 @@ int main(int argc, char **argv)
 	}
 	write(1, "\n", 1);
 	return 0;
-}
+} */
 
 int main(int ac, char **av){
 	char *firstword;
 
+	if (ac < 2)
+		return 0;
 	while (*av[1] && (*av[1] == ' ' || (*av[1] >= 9 && *av[1] <= 13)))
 		av[1]++;
 	firstword = av[1];
-	while (*av[1] && (*av[1] != ' ' && *av[1] <= 9 && *av[1] >= 13))
+	while (*av[1] && (*av[1] != ' ' && (*av[1] < 9 || *av[1] > 13)))
 		av[1]++;
-	while (*av){
-		while (*av && (*av == ' ' || *av >= 9 || *av <= 13))
-			av++;
+	while (*av[1]){
+		while (*av[1] && (*av[1] == ' ' || (*av[1] >= 9 && *av[1] <= 13)))
+			av[1]++;
+		while (*av[1] && (*av[1] != ' ' && (*av[1] < 9 || *av[1] > 13)))
+			write(1, &*av[1]++, 1);
+		// if (*av[1] && *av[1] == ' ' && (*av[1] >= 9 && *av[1] <= 13))
+		write(1, " ", 1);
 	}
+	while (*firstword && (*firstword != ' ' && (*firstword <= 9 || *firstword >= 13)))
+		write(1, &*firstword++, 1);
+	write(1, "\n", 1);
+	return 0;
 }
