54f87356b5853091e6b356a5f20a02c4c1c9bd60
Adrien 42 <adr.bidaux@gmail.com>
2025-02-22 20:19:07 +0100

    debut split

---
+++ 2-02.pushswap/ps_split.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ps_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 18:37:35 by abidaux           #+#    #+#             */
+/*   Updated: 2025/02/22 19:16:23 by abidaux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	count_words(char *s, char c)
+{
+	int		count;
+	bool	inside_word;
+
+	count = 0;
+	while (*s)
+	{
+		inside_word = false;
+		while (*s == c)
+			s++;
+		while (*s != c && *s)
+		{
+			if (!inside_word)
+			{
+				++count;
+				inside_word = true;
+			}
+			++s;
+		}
+	}
+	return (count);
+}
--- 2-02.pushswap/push_swap.c
+++ 2-02.pushswap/push_swap.c
@@ -6,11 +6,56 @@
 /*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 16:28:07 by abidaux           #+#    #+#             */
-/*   Updated: 2025/02/22 16:39:31 by abidaux          ###   ########.fr       */
+/*   Updated: 2025/02/22 18:37:19 by abidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	sa(t_stack_node **head)
+{
+	t_stack_node	*temp_head;
+	t_stack_node	*temp_3;
+
+	if (!head || !*head || !((*head)->next))
+		return ;
+	temp_head = *head;
+	temp_3 = (*head)->next->next;
+	(*head) = (*head)->next;
+	(*head)->next = temp_head;
+	(*head)->next->next = temp_3;
+}
+
+int	stack_len(t_stack_node **head)
+{
+	t_stack_node	*temp;
+	int				count;
+
+	temp = *head;
+	count = 0;
+	while (temp)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
+
+bool	stack_sorted(t_stack_node **head)
+{
+	t_stack_node	*temp;
+
+	temp = head;
+	while (temp)
+	{
+		if (!temp->next)
+			return (true);
+		if (temp->nbr > temp->next->nbr)
+			return (false);
+		temp = temp->next;
+	}
+	return (true);
+}
 
 int main(int ac, char **av)
 {
@@ -22,7 +67,7 @@ int main(int ac, char **av)
 	if (ac == 1 || (ac == 2 && !av[1][0]))
 		return (1);
 	else if (ac == 2)
-		av = ft_split(av[1], ' ');
+		av = ps_split(av[1], ' ');
 	init_stack_a(&a, av + 1);
 	if (!stack_sorted(a))
 	{
